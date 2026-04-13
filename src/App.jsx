a59c815fb3b0563201827626273fe60c6f790c09
kaykyrugani <kaykyrugani@gmail.com>
2026-04-13 18:24:36 -0300

    att relaod pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import Layout from './components/layout/Layout'
+import ScrollToTop from './components/layout/ScrollToTop'
 import AppRoutes from './routes/AppRoutes'
 import './styles/globals.css'
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Layout>
           <AppRoutes />
         </Layout>
