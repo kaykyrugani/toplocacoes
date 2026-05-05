7cebec702ef53c2fb6d8ad15415226e7c029ec9a
kaykyrugani <kaykyrugani@gmail.com>
2026-05-05 16:40:01 -0300

    att rodarbuild

--- src/components/sections/DownloadsSection.jsx
+++ src/components/sections/DownloadsSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from '../ui/Container'
 import SectionTitle from '../ui/SectionTitle'
-import './DownloadsSection.css'
+import '../../../DownloadsSection.css'
 
 const DownloadsSection = ({ content }) => {
   return (
